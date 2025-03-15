f9b9888401d100b568ce5f17f987e4afab7f1915
IhorSliepchenko1 <igorslepchenko.cc@gmail.com>
2025-03-15 16:07:37 +0200

    fix

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,14 +1,14 @@
 import { Divider } from "@mantine/core"
 import { AddUserForm } from "../app/components/form/add-user-form"
 import { UserComponent } from "../app/components/features/user/user-component"
-// import { CreateRaport } from "../app/components/button/create-raport"
+import { CreateRaport } from "../app/components/button/create-raport"
 
 export const Users = () => {
      return (
           <>
-               {/* <div className="flex justify-end">
+               <div className="flex justify-end">
                     <CreateRaport />
-               </div> */}
+               </div>
                <AddUserForm />
                <Divider my="sm" />
                <UserComponent />
